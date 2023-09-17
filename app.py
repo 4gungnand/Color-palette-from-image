e089c02150b628c4ca9da7732f347f0fe4c9d7c2
I Gusti Agung Premananda <premananda2003@gmail.com>
2023-09-17 14:12:24 +0700

    First commit

---
+++ app.py
@@ -0,0 +1,46 @@
+from flask import Flask, render_template, request
+from PIL import Image
+import numpy as np
+import matplotlib.pyplot as plt
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if request.method == "POST":
+        img = Image.open(request.files["file"])
+        dominant_colors = get_dominant_colors(img)
+        plot_colors(dominant_colors)
+        return "Color palette generated. Check the server directory for 'output.png'"
+    return '''
+        <form action="/" method="post" enctype="multipart/form-data">
+            <input type="file" name="file">
+            <input type="submit" value="Upload">
+        </form>
+    '''
+
+def get_dominant_colors(img, n_colors=5):
+    img = img.resize((100, 100))  # resizing to speed up processing time
+    data = np.array(img)
+
+    if data.shape[-1] == 4:  # If the image has an Alpha channel
+        data = data.reshape(-1, 4)
+        data = [tuple(item) for item in data if item[3] != 0]  # remove transparent pixels
+    else:
+        data = data.reshape(-1, 3)
+
+    unique, counts = np.unique(data, axis=0, return_counts=True)
+    sorted_indices = np.argsort(-counts)
+    dominant_colors = unique[sorted_indices][:n_colors]
+
+    return dominant_colors
+
+def plot_colors(colors):
+    fig, ax = plt.subplots(1, figsize=(5, 2), subplot_kw=dict(xticks=[], yticks=[], frame_on=False))
+    for sp in ax.spines.values():
+        sp.set_visible(False)
+    ax.imshow([colors], aspect='auto')
+    plt.savefig("output.png")
+
+if __name__ == "__main__":
+    app.run(debug=True)
